d82b180c76aee1e07ad87733c97c069ccad183c9
Philipp Kaiser <philipp.kaiser@frachtwerk.de>
2023-11-17 18:41:33 +0100

    feat: add prisma and set count for api calls

--- app/api/music/route.ts
+++ app/api/music/route.ts
@@ -1,3 +1,4 @@
+import { checkApiLimit, increaseApiLimit } from '@/lib/api-limit'
 import { auth } from '@clerk/nextjs'
 import { NextResponse } from 'next/server'
 import Replicate from 'replicate'
@@ -20,11 +21,19 @@ export async function POST(req: Request) {
       return new NextResponse('Prompt not found', { status: 400 })
     }
 
+    const freeTrial = await checkApiLimit()
+
+    if (!freeTrial) {
+      return new NextResponse('Free trial limit reached', { status: 403 })
+    }
+
     const response = await replicate.run('meta/musicgen:7a76a8258b23fae65c5a22debb8841d1d7e816b75c2f24218cd2bd8573787906', {
       input: {
         model_version: prompt,
       },
     })
+
+    await increaseApiLimit()
 
     return NextResponse.json(response)
   } catch (error) {
--- components/loader.tsx
+++ components/loader.tsx
@@ -4,7 +4,7 @@ export function Loader() {
   return (
     <div className='flex flex-col items-center justify-center h-full gap-y-4'>
       <div className='relative w-10 h-10 animate-spin'>
-        <Image alt='logo' fill src='/next.svg' />
+        <Image alt='logo' fill src='/bonsai.png' />
       </div>
 
       <p className='text-sm text-muted-foreground'>Bonsai is thinking...</p>
