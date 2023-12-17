eb7460470f7ade8507e32f2fcd58cbc475d35caf
Philipp Kaiser <philipp.kaiser@frachtwerk.de>
2023-12-17 15:50:59 +0100

    feat: add landing page

---
+++ app/(landing)/layout.tsx
@@ -0,0 +1,7 @@
+export default function LandingLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <main className='h-full bg-[#111827] overflow-auto'>
+      <div className='w-full h-full max-w-screen-xl mx-auto'>{children}</div>
+    </main>
+  )
+}
---
+++ components/landing-content.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+import test from 'node:test'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+
+const testimonials = [
+  { name: 'Philipp', avatar: 'A', title: 'Software Engineer', description: "This is the best application I've used" },
+  {
+    name: 'Olaf',
+    avatar: 'O',
+    title: 'Craftsman',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+  },
+  {
+    name: 'Miles',
+    avatar: 'M',
+    title: 'Firefighter',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+  },
+  {
+    name: 'Ken',
+    avatar: 'K',
+    title: 'Explorer',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+  },
+]
+
+export default function LandingContent() {
+  return (
+    <div className='px-10 pb-20'>
+      <h2 className='mb-10 text-4xl font-extrabold text-center text-white'>Testimonials</h2>
+
+      <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+        {testimonials.map(testimonial => (
+          <Card key={testimonial.description} className='bg-[#192339] border-none text-white'>
+            <CardHeader>
+              <CardTitle className='flex items-center gap-x-2'>
+                <div>
+                  <p className='text-lg'>{testimonial.name}</p>
+                  <p className='text-sm text-zinc-400'>{testimonial.title}</p>
+                </div>
+              </CardTitle>
+              <CardContent className='px-0 pt-4'>{testimonial.description}</CardContent>
+            </CardHeader>
+          </Card>
+        ))}
+      </div>
+    </div>
+  )
+}
